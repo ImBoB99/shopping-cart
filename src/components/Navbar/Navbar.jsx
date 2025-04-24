7dd559302a401cb3c582b63c7a1ca9c69a32b6a5
Dominik Ubuntu <119432963+ImBoB99@users.noreply.github.com>
2025-04-24 13:41:25 +0200

    added lucide icons, added shopping cart icon to navbar and styled it

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { ShoppingCart } from "lucide-react";
+
 
 function Navbar() {
   return (
     <nav className={styles.navContainer}>
-      <div className={styles.navContainerInner}>
         <h1 className={styles.brandTitle}>
           <span className={styles.odin}>Odin</span>
           <span className={styles.buyGradient}>Buy</span>
@@ -12,9 +13,8 @@ function Navbar() {
         <div className={styles.navLinks}>
           <Link to="/">Home</Link>
           <Link to="/shop">Shop</Link>
-          <Link to="/shopping-cart">ShoppingCart</Link>
         </div>
-      </div>
+        <Link to="/shopping-cart"><ShoppingCart className={styles.cartIcon} /></Link>
     </nav>
   );
 }
